534102eae268c2f72241d380488466a4762a153c
GantGanz <anggerelegant7@gmail.com>
2021-02-03 09:44:15 +0700

    auth 5

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,1 +1,17 @@
-// signin
+// sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get account info
+    const nama = signupForm['signup-nama'].value;
+    const nip = signupForm['signup-nip'].value;
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // signup the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        signupForm.reset();
+    });
+});
