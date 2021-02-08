43f44b672e37c99f9c9debff994f38e776d0b785
GantGanz <anggerelegant7@gmail.com>
2021-02-08 19:35:33 +0700

    9

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,8 @@
+// get data
+db.collection('presensi').get().then(snapshot => {
+    setupPresensi(snapshot.docs);
+})
+
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
     if (user) {
---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const presensiList = document.querySelector('.presensiList');
+
+// setup presensi
+const setupPresensi = (data) => {
+    let html = '';
+    // let row = 1;
+    data.forEach(doc => {
+        const presensi = doc.data();
+        const td = `
+        <tr>
+            <td>${presensi.tanggal}</td>
+            <td>${presensi.waktu}</td>
+            <td>${presensi.foto}</td>
+        </tr>
+        `;
+        html += td;
+        // row++;
+    });
+    presensiList.innerHTML = html;
+}
