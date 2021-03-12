951ee9389bcfc1688f4ca820f4c3b56a3ff54057
GantGanz <anggerelegant7@gmail.com>
2021-03-12 17:46:26 +0700

    pwa 10

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', evt => {
     console.log('service worker has been installed');
 });
+
+// active event
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated');
+});
