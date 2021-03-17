4bdc5b8e088e2ecddf42d5979452bf956000f55a
GantGanz <anggerelegant7@gmail.com>
2021-03-17 14:02:17 +0700

    pwa 20

--- sw.js
+++ sw.js
@@ -62,6 +62,10 @@ self.addEventListener('fetch', evt => {
                     return fetchRes;
                 });
             });
-        }).catch(() => caches.match('fallback.html'))
-    )
+        }).catch(() => {
+            if (evt.request.url.indexOf('.html') > -1) {
+                return caches.match('fallback.html');
+            }
+        })
+    );
 });
