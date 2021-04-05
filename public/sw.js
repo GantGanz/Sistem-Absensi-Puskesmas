20833eed1e6df6b3c12a41dd3fe39c67088c02fc
GantGanz <anggerelegant7@gmail.com>
2021-04-05 11:48:55 +0700

    scroll to scrol stop for phone

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v13';
-const dynamicCacheName = 'site-dynamic-v13';
+const staticCacheName = 'site-static-v14';
+const dynamicCacheName = 'site-dynamic-v14';
 const assets = [
     '/',
     'css/bootstrap.min.css',
